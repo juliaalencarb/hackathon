9486a3da41b1482f1dcbbad28c3d8a7e04446fe1
bossSteve03 <stotev0@gmail.com>
2023-03-24 10:56:57 +0000

    made controllers for all too though some need fixing. check client tester on getAll func

---
+++ controllers/diaryController.js
@@ -0,0 +1,52 @@
+const DiaryEntry = require('../models/Diary');
+const DiaryService = require('../models/Diary');
+
+async function showAll(req, res) {
+    try {
+        const data = await DiaryService.getAll()
+        res.status(200).send(data)
+    } catch(err) {
+        res.status(404).json({"error": err.message})
+    }
+}
+
+async function create(req, res) {
+    try {
+        const response = await DiaryService.create(req.body)
+        res.status(201).send(response)
+    } catch(err) {
+        res.status(400).json({"error": err.message})
+    }
+}
+
+async function show(req, res) {
+    try {
+        const id = parseInt(req.params.id)
+        const data = await DiaryService.show(id)
+        res.status(200).send(data)
+    } catch(err) {
+        res.status(404).json({"error": err.message})
+    }
+}
+
+async function update(req, res) {
+    try {
+        const id = parseInt(req.params.id)
+        const response = await DiaryService.update(id, data)
+        res.status(200).send(response)
+    } catch(err) {
+        res.status(404).json({"error": err.message})
+    }
+}
+
+async function destroy(req, res) {
+    try {
+        const id = parseInt(req.params.id)
+        const response = await DiaryService.destroy(id)
+        res.status(200).send(response)
+    } catch(err) {
+        res.status(404).json({"error": err.message})
+    }
+}
+
+module.exports = {showAll, create, show, update, destroy}
--- routers/diaryRouter.js
+++ routers/diaryRouter.js
@@ -3,4 +3,10 @@ const diaryController = require('../controllers/diaryController.js');
 
 const diaryRouter = Router();
 
+diaryRouter.get('/', diaryController.showAll)
+diaryRouter.post('/', diaryController.create)
+diaryRouter.get('/:id', diaryController.show)
+diaryRouter.patch('/:id', diaryController.update)
+diaryRouter.delete('/:id', diaryController.destroy)
+
 module.exports = diaryRouter;
